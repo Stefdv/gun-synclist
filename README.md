a661fad18a1988aaa72533bb42e9cd1cc6516139
Stefdv <stefdv2@hotmail.com>
2018-04-13 22:02:45 +0200

    Gun module
    Returns Array of a set on init.
    Returns soul,index and full node on every change

--- README.md
+++ README.md
@@ -1,1 +1,72 @@
 # gun-synclist
+
+## Intro
+
+`gun-synclist` is a module for Gun(^0.9.x) using `Gun.chain`.
+It is basicly the same as the  `open()` module included in Gun/lib. But where `open()` gives you the full document ( Object ) on every change, `gun-synclist` behaves a little different.
+
+* On initial run `gun-synclist` will return the full `set` as an Array!
+  ( including the internally used 'lookup' { soul:index })
+* On every change after the initial run it only returns the changed node.
+  but...It gives you the soul of the node, the index in the Array and the full
+  node ( Yeah...including nested properties )
+
+  eg: { soul: "AbCd123..." ,idx: 112, node: {...}
+
+### Why?
+If you have a list with a lot of items you don't want to rebuild your DOM on every change. Instead you only need to change the changed item.
+
+eg: `items.splice( idx,1,node )`
+
+[What you could do with gun-synclist]( https://codepen.io/Stefdv/pen/WzJwqq )
+
+## Setup
+
+##### Prerequisites
+You need Gun ^0.9.x
+```
+ npm install gun-synclist
+```
+
+### Node.js
+```
+var Gun = require('gun');
+require('gun-synclist');
+```
+
+### Browser
+Just include it as a script tag, and gun-synclist takes care of the rest.
+```
+<!-- AFTER you load Gun -->
+<script src="node_modules/gun-synclist/gun-synclist.js"></script>
+```
+
+## Example
+Not much to it..
+
+```
+var items;
+var gun = Gun();
+var CONTACTS = gun.get('CONTACTS');
+CONTACTS.set( { name:'Stefdv',age: 44,profile:{email:"myemail@home.nl"}} )
+
+CONTACTS.synclist( obj => {
+  if(obj.list) {
+    // initial only!
+    items = obj.list;
+  } 
+  if(obj.node) {
+    // on every change.
+    items.splice(obj.idx,1,obj.node)
+  }
+})
+```
+
+## Note: 
+`gun-synclist` will automatically load everything it can find on the context. This may sound convenient, but may be unnecessary and excessive - resulting in more bandwidth and slower load times for larger data. It could also result in your entire database being loaded, if your app is highly interconnected.
+Therefore `gun-synclist` should be used on a `set`. 
+
+ That being said... tell me what you want to do and - maybe- i can advise you. I'm not much of an email reader so ping me in [gitter](https://gitter.im/amark/gun) @Stefdv
+ 
+### Credits
+Thanks to Mark Nadal for writing Gun and for helping out whenever i need.
---
+++ gun-synclist.js
@@ -0,0 +1,80 @@
+/**
+ * gun-synclist
+ * @author  S.J.J. de Vries ( Stefdv2@hotmail.com)
+ * @gitter @Stefdv
+ * @purpose 
+ * - on init it will return your full set as an Array, including a 'lookup' Object.
+ * - on every change after initial run it reurns the full changed node, its soul and 
+ *   its index in the initial Array. eg: {soul:"AbCd123....", idx: 121, {<your full node>}}
+ * - even on nested property changes it will still return the node soul ( not the soul of the nested object);
+ * 
+ * @why
+ * - No need to rebuild the whole list in DOM when one property changes...just use the index to splice the item.
+ * 
+ * ( i used .open() from Gun/lib/open.js as a base, tx those developers.)
+ */
+
+;(function(){
+  if(typeof window !== "undefined"){
+    var Gun = window.Gun;
+  } else {
+    var Gun = require('gun/gun');
+  }
+
+  Gun.chain.synclist = function(cb, opt, at){
+    opt = opt || {};
+    opt.doc = opt.doc || {};                // used to build the full set
+    opt.ids = opt.ids || {};                // keep track of processed ids
+    opt.init = opt.init || true;            // if we have opt.init...init is finished ;p
+    opt.any = opt.any || cb;                // users callback function
+    opt.list = opt.list || [];              // Array build from opt.doc
+    opt.lookup = opt.lookup || {};          // lookup soul/index in opt.list
+    opt.owner = opt.owner || null;          // soul of the node the change happened on
+    
+    this.on(function(data, key, ctx){
+      delete ((data = Gun.obj.copy(data))||{})._;
+        if(this.back(1)._.get) {
+          opt.root = this.back(1)._.get;
+          opt.prop = this._.get  ;
+          if(opt.doc[opt.root]){ opt.owner = opt.root; }
+          else if(opt.doc[opt.prop]) { opt.owner = opt.prop; } 
+        } 
+
+        clearTimeout(opt.to);
+          opt.to = setTimeout(function(){
+            if(!opt.any){ return };
+            if(opt.init) {
+              let list =Object.keys(opt.doc).map( (soul,idx) => {
+                  opt.lookup[soul] = idx;
+                  opt.doc[soul]._soul = soul;
+                  return opt.doc[soul]
+              });
+              opt.any.call(opt.at.gun,{list:list,lookup:opt.lookup});
+            } else {
+              opt.any.call(opt.at.gun,{soul:opt.owner,
+                                      idx:opt.lookup[opt.owner],
+                                      node:opt.doc[opt.owner]}
+                          );
+            } 
+            opt.init = false;
+          }, opt.wait || 1);
+      
+          opt.at = opt.at || ctx;
+          opt.key = opt.key || key;
+          var tmp = this, id,idx;
+          Gun.obj.map(data, function(val, key){
+            id = Gun.val.rel.is(val);
+            if(!id) { 
+              (at || opt.doc)[key] = val;
+              return;
+            } 
+            if(opt.ids[id]){
+              (at || opt.doc)[key] = opt.ids[id];
+              return;
+            }
+            opt.ids[id] = ( at || opt.doc )[key] = {};
+            tmp.get(key).synclist(opt.any, opt, opt.ids[id]);
+          });
+        },true)
+      }
+    }());
